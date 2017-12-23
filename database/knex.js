475a5d85fa3eff77a2d50b3234b5bb51162996c0
rockyyyr <rockyrobson@hotmail.com>
2017-12-23 14:52:45 -0800

    changed database env variable name

--- database/knex.js
+++ database/knex.js
@@ -1,6 +1,6 @@
 module.exports = require('knex')({
   client: 'mysql2',
-  connection: process.env.DATABASE_URL || require('./database.json'),
+  connection: process.env.JAWSDB_URL || require('./database.json'),
   pool: {
     min: 0,
     max: 7
